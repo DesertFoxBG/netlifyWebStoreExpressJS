6e7b499c70325f15755c723147bef64e04c4aaa3
DesertFoxBG <jordan1711@abv.bg>
2020-03-12 15:01:11 +0200

    check

--- express/server.js
+++ express/server.js
@@ -8,6 +8,7 @@ const bodyParser = require('body-parser');
 const router = express.Router();
 
 router.get('/', (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
   res.end();
 });
 
