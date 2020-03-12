c728c4bc135b66c4f1d5bd37fdc4c7e4313019ae
DesertFoxBG <jordan1711@abv.bg>
2020-03-12 14:23:41 +0200

    check

--- express/server.js
+++ express/server.js
@@ -7,28 +7,34 @@ const bodyParser = require('body-parser');
 
 const router = express.Router();
 
-app.get('/', (req, res) => {
-  res.redirect(req.baseUrl + '/home');
+app.use('/', (req, res) => {
+  res.render(path.join(__dirname, '../index.ejs'), {
+    title: "Home"
+  })
 });
+
+// app.get('/', (req, res) => {
+//   res.redirect(req.baseUrl + '/home');
+// });
 
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 
 router.post('/', (req, res) => res.json({ postBody: req.body }));
 
-app.use(bodyParser.json());
-app.use(express.static('public'));
-app.set('view engine', 'ejs');
-app.use('/.netlify/functions/server', router);  // path must route to lambda
-app.use('/home', (req, res) => {
-  res.render(path.join(__dirname, '../public/views/index.ejs'), {
-    title: "Home"
-  })
-});
-app.use('/products/websiteplugins', (req, res) => {
-  res.render('../public/views/products/websiteplugins.ejs', {
-    title: "Website Plugins"
-  })
-});
+// app.use(bodyParser.json());
+// app.use(express.static('public'));
+// app.set('view engine', 'ejs');
+// app.use('/.netlify/functions/server', router);  // path must route to lambda
+// app.use('/home', (req, res) => {
+//   res.render(path.join(__dirname, '../public/views/index.ejs'), {
+//     title: "Home"
+//   })
+// });
+// app.use('/products/websiteplugins', (req, res) => {
+//   res.render('../public/views/products/websiteplugins.ejs', {
+//     title: "Website Plugins"
+//   })
+// });
 
 module.exports = app;
 module.exports.handler = serverless(app);
